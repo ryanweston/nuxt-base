7f0861a3d842c6587923f7384e91f12049ee615b
Ryan Weston <ryanlweston@gmail.com>
2025-03-02 20:34:43 +0000

    fix: example route

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@ defineOgImageComponent('Default')
 <template>
   <div>
     <NuxtLink
-      to="/example/example"
+      to="/example"
       class="border flex items-center gap-1 text-sm px-2 hover:bg-zinc-100 py-1 rounded dark:border-zinc-600 border-zinc-300 dark:hover-bg-zinc-800"
     >
       View markdown route <Icon name="carbon:arrow-right" />
